03e37bb7f3e048d697fb0d0b5688da19588282fc
Jt Miclat <jtcmic@gmail.com>
2019-01-14 23:04:55 +0800

    Removed bin files and migrated to Makefile

--- bin/upload_dropbox.sh
+++
@@ -1,3 +0,0 @@
-IMAGE=kennethreitz/pipenv
-exec docker run --rm  -e DROPBOX_TOKEN=$DROPBOX_TOKEN -v "$PWD:/app" "$IMAGE" sh -c "pipenv install && pipenv run python upload_dropbox.py --source=Resume/Resume.pdf --target=/Resume.pdf"
-
